0e05def6552ab02b69191c627993784512c1bd2d
juliperez3 <juleipe@hotmail.com>
2025-10-20 17:59:30 -0300

    ttttt

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+// postcss.config.mjs
+export default {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
   },
 };
 
-export default config;
